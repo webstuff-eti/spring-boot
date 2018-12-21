91e7a1a1f8e0e83af8b0f29a70e636e58c874fe4
Tiago Tibães <tibaestiago@gmail.com>
2018-12-21 18:58:18 -0200

    Aplicando TDD para criar ambientes de homologação e desenvolvimentos

--- ExtratosLancamentosConta/src/test/java/br/eti/webstuff/api/ExtratosLancamentosContaApplicationTests.java
+++ ExtratosLancamentosConta/src/test/java/br/eti/webstuff/api/ExtratosLancamentosContaApplicationTests.java
@@ -1,16 +1,28 @@
 package br.eti.webstuff.api;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class ExtratosLancamentosContaApplicationTests {
+	
+	@Value("${parametro.configuracao.ambiente}")
+	private String ambiente;
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void verificaContextoDeAmbiente () {
+		Assert.assertEquals("desenvolvimento", ambiente);
 	}
 
 }
