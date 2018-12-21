73c6422ee2421df6a967d57277396a91795ce9ed
Tiago Tibães <tibaestiago@gmail.com>
2018-12-21 16:48:25 -0200

    projeto-base-desenvolvido

---
+++ ExtratosLancamentosConta/src/main/java/br/eti/webstuff/api/ExtratosLancamentosContaApplication.java
@@ -0,0 +1,14 @@
+package br.eti.webstuff.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtratosLancamentosContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtratosLancamentosContaApplication.class, args);
+	}
+
+}
+
